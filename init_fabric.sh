749b9fab0453c35ef8103729a59262b79fb9e3b5
Zhao Zhenlong <zzl_1164@126.com>
2014-05-12 15:12:32 +0800

    some git config

--- init_fabric.sh
+++ init_fabric.sh
@@ -3,3 +3,9 @@ yum install -y python-pip python-devel
 pip install fabric
 pip install tentakel
 cp ./tentakel.conf /etc/tentakel.conf
+
+git config --global user.name "Zhao Zhenlong"
+git config --global user.email zzl_1164@126.com
+git config --global alias.st status
+git config --global alias.br branch 
+git config --global alias.dif diff 
