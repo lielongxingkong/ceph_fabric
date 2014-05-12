e686aa1cc23b0f39d46d4d6ade5dcbce8e9ca1f5
Zhao Zhenlong <zhaozhl@inspur.com>
2014-05-12 18:07:43 +0800

    add install ntp server

--- init_fabric.sh
+++ init_fabric.sh
@@ -9,3 +9,8 @@ git config --global user.email zzl_1164@126.com
 git config --global alias.st status
 git config --global alias.br branch 
 git config --global alias.dif diff 
+
+yum install -y ntp
+echo "server 127.127.1.0\nrestrict 192.168.0.0 mask 255.255.0.0 nomodify\nfudge 127.127.1.0 stratum 0 stratum" >> /etc/ntp.conf
+service ntpd start 
+
